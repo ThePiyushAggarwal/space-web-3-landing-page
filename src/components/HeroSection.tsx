be279ceeb555b5bf5abe2cd81ba283ba61be2b7e
Piyush Aggarwal <piyushaggarwal801@gmail.com>
2023-05-13 23:06:47 +0530

    feat: added Hero Section

---
+++ src/components/HeroSection.tsx
@@ -0,0 +1,28 @@
+import { Button } from "./Button"
+
+function HeroSection() {
+	return (
+		<div className="grid py-24 text-secondary md:grid-cols-2">
+			<div className="mb-24 md:mb-0">
+				<h1 className="mb-8 text-7xl font-extrabold uppercase md:text-8xl">
+					Discover Collect Sell Digital NFT.
+				</h1>
+				<h3 className="mb-8 text-xl text-ternary md:text-2xl">
+					Generate 10,000+ NFT Collection Only by uploading the layers, and get
+					ready to sell your collection.
+				</h3>
+				<div className="flex gap-4">
+					<Button variant="primary">Get Started</Button>
+					<Button variant="secondary">Join Discord</Button>
+				</div>
+			</div>
+			<div className="relative flex items-center justify-center">
+				<img src="./image.webp" className="z-20" />
+				<div className="absolute bottom-20 z-10 rounded-full bg-black p-40 opacity-80 md:p-48" />
+				<div className="absolute bottom-20 z-0 rounded-full bg-black p-56 opacity-10 md:p-64" />
+			</div>
+		</div>
+	)
+}
+
+export { HeroSection }
