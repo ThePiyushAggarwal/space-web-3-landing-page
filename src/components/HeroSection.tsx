bdf16ed6788e4f267686efb0ba5b9a9bc28e5867
Piyush Aggarwal <piyushaggarwal801@gmail.com>
2023-05-14 11:21:46 +0530

    fix: responsiveness fix in hero section

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -7,11 +7,11 @@ function HeroSection() {
 				<h1 className="mb-8 text-7xl font-extrabold uppercase md:text-8xl">
 					Discover Collect Sell Digital NFT.
 				</h1>
-				<h3 className="mb-8 text-xl text-ternary md:text-2xl">
+				<h3 className="mb-8 text-2xl text-ternary md:text-3xl">
 					Generate 10,000+ NFT Collection Only by uploading the layers, and get
 					ready to sell your collection.
 				</h3>
-				<div className="flex gap-4">
+				<div className="flex justify-evenly gap-4 md:justify-normal">
 					<Button variant="primary">Get Started</Button>
 					<Button variant="secondary">Join Discord</Button>
 				</div>
